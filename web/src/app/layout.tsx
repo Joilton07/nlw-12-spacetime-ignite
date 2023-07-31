84da60bff49d7c1520f810eb4bf00f1ae2ec51f5
Joilton Alvino <joiltonEstTrab@outlook.com>
2023-07-31 18:08:19 -0300

    NLW Spacetime aula 4

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import {
-  Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
+  Roboto_Flex as Roboto,
 } from 'next/font/google'
+import { cookies } from 'next/headers'
 import { ReactNode } from 'react'
+
+import { Copyright } from '@/Components/Copyright'
+import { Hero } from '@/Components/Hero'
+import { SignIn } from '@/Components/SignIn'
+import { Profile } from '@/Components/profile'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({
@@ -20,12 +26,35 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+            {/* Stripes */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {/* Sign in */}
+            {isAuthenticated ? <Profile /> : <SignIn />}
+
+            {/* Hero */}
+            <Hero />
+            {/* Copyright */}
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
